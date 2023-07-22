8daa71d00e0a7ff49008c1f2bc1edfae1592c562
Anupamaraie <-->
2023-07-22 23:16:02 +0545

    Navbar

---
+++ Navbar/app.js
@@ -0,0 +1,16 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+const icons = document.querySelector('.icons');
+
+navToggle.addEventListener('click', function(){
+    // console.log(icons.classList.contains('toggle')
+    // );
+
+    if (icons.classList.contains("toggle")){
+        icons.classList.remove("toggle");
+    }
+    else{
+        icons.classList.add("toggle");
+        icons.body.style.display = 'block';
+    }
+})
